741c56874aa8e80b301ccd766806c1549e4c72be
galiardi <66950896+galiardi@users.noreply.github.com>
2023-05-11 17:59:04 -0400

    ...

--- main.js
+++ main.js
@@ -3,12 +3,16 @@ const birthdate = prompt('Ingrese Fecha de nacimiento (Ej: 19-04-2023)');
 global(birthdate);
 
 function global(date) {
+  console.log(date);
   const formattedDate = date.split('-').reverse().join('-');
+  console.log(formattedDate);
   const dateObj = new Date(formattedDate);
+  console.log(dateObj);
   const dayOfBirthdate = getDayOfBirthdate(dateObj);
   getAge(dateObj);
   totalDays(dateObj);
-  getRemainingDaysForBirthday(dateObj);
+  const remainingDaysForBirthday = getRemainingDaysForBirthday(dateObj);
+  console.log(remainingDaysForBirthday);
 }
 
 //Jonathan
@@ -58,23 +62,50 @@ function totalDays(obj) {
   return valor / (1000 * 60 * 60 * 24);
 }
 
-function getRemainingDaysForBirthday(obj) {
-  const now = Date.now();
-
-  const birthday = {
-    month: obj.getMonth(),
-    date: obj.getDate(),
-  };
+function getRemainingDaysForBirthday(dateObj) {
+  const now = new Date();
 
   const today = {
     month: now.getMonth(),
     date: now.getDate(),
+    year: now.getFullYear(),
+    miliseconds: Date.now(),
+  };
+
+  const birthday = {
+    month: dateObj.getMonth(),
+    date: dateObj.getDate(),
+    year: undefined,
+    miliseconds: undefined,
   };
 
   const remaining = {
     month: birthday.month - today.month,
     date: birthday.date - today.date,
+    miliseconds: undefined,
   };
 
-  console.log(remaining);
+  const birthdayIsOver =
+    remaining.month < 0 || (remaining.month === 0 && remaining.date < 0);
+
+  if (birthdayIsOver) {
+    birthday.year = today.year + 1;
+  } else {
+    birthday.year = today.year;
+  }
+  const formattedBirthday = `${birthday.year}-${birthday.month + 1}-${
+    birthday.date
+  }`;
+
+  birthday.miliseconds = Date.parse(formattedBirthday);
+
+  console.log(birthday);
+
+  remaining.miliseconds = birthday.miliseconds - today.miliseconds;
+
+  const remainigDaysForBirthday = Math.floor(
+    remaining.miliseconds / (24 * 60 * 60 * 1000)
+  );
+
+  return remainigDaysForBirthday;
 }
