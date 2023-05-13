fd374ef6e414d3f27853f5c54d8db18266f1eee9
galiardi <66950896+galiardi@users.noreply.github.com>
2023-05-13 07:34:39 -0400

    Cambio de nombre a los archivos js.

--- seccionB.js
+++ seccionB.js
@@ -4,11 +4,14 @@ ingresoOrganizacion(ingresoOrg);
 function ingresoOrganizacion(ingreso, actual){
 
   const [dia, mes, ano] = ingreso.split('-');
-//   console.log(dia, mes, ano);
+
   const dateObj = new Date(parseInt(ano), parseInt(mes) - 1, parseInt(dia));
   console.log(dateObj);
-  document.write(`<br/> Su permanencia en la organización es de: ${totalDays(dateObj)} días. <br/>
-  Su permanencia en la organización es de: ${absMonth(dateObj)} meses.<br/>
-  Su permanencia en la organización es de: ${getAge(dateObj).edadAno} años y ${getAge(dateObj).edadMes} meses y ${getAge(dateObj).edadDias} días.<br/>
+
+  const { edadAno: years, edadMes: months, edadDias: days} = getAge(dateObj);
+  
+  document.write(`<br/> <br/> Su permanencia en la organización es de: ${totalDays(dateObj)} días. <br/>
+  Su permanencia en la organización es de: ${getAbsMonths(dateObj)} meses.<br/>
+  Su permanencia en la organización es de: ${years} años y ${months} meses y ${days} días.<br/>
   Para completar el año de permanencia faltan: ${getRemainingDaysForBirthday(dateObj)}`);
 }
